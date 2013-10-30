20f17c3ddbcc137a053eb0dd01acb97d3ce422e1
David Friermor, Meagan Cooney and Michael McCormick <pair+dfriermor+meagan+mc@pivotallabs.com>
2013-10-29 19:54:29 -0700

    Adds styleguide resource/controller/show action

---
+++ app/controllers/styleguides_controller.rb
@@ -0,0 +1,3 @@
+class StyleguidesController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LiveStyleGuide::Application.routes.draw do
+  resource :styleguide, :only => :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
