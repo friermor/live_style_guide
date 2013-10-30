83c26943429af5822ad3bfcf16c1f959870a0d85
David Friermor, Meagan Cooney and Michael McCormick <pair+dfriermor+meagan+mc@pivotallabs.com>
2013-10-29 19:39:26 -0700

    Hello Rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LiveStyleGuide::Application.config.secret_key_base = '6c31bcb44722e959e78f2bfc29723f955cc0d6679a49ceddcebabaecdc5cee740e04ce168238f71d719d5731b8320445631a78320b63f01be09017434e97ac24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LiveStyleGuide::Application.config.session_store :cookie_store, key: '_live_style_guide_session'
